4a7523fb75c4cc2dc059ead56906999ee6732f13
camille.viot <cviot@acreos.eu>
2022-03-11 14:39:46 +0100

    clean up sample code

--- TestBannedAPI/Program.cs
+++ TestBannedAPI/Program.cs
@@ -1,7 +1,26 @@
-﻿var myObj = new MyObject();
-MyObject.AMethod(myObj);
-if (myObj)
+﻿public class MyObject
 {
-    Console.WriteLine("test 1");
+    public static implicit operator bool(MyObject obj)
+    {
+        return obj != null;
+    }
+
+    public static void AMethod(MyObject obj)
+    {
+        Console.WriteLine("hi");
+    }
 }
-Console.WriteLine("test");
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var myObj = new MyObject();
+        MyObject.AMethod(myObj);
+        if (myObj)
+        {
+            Console.WriteLine("test 1");
+        }
+        Console.WriteLine("test");
+    }
+}
