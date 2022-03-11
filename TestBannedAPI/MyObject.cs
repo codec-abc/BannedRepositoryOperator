e39f0f65bca0b0346a4a95e41b48ef02256049cd
camille.viot <cviot@acreos.eu>
2022-03-11 14:38:13 +0100

    first commit

---
+++ TestBannedAPI/MyObject.cs
@@ -0,0 +1,12 @@
+﻿public class MyObject
+{
+    public static implicit operator bool(MyObject obj)
+    {
+        return obj != null;
+    }
+
+    public static void AMethod(MyObject obj)
+    {
+        Console.WriteLine("hi");
+    }
+}
---
+++ TestBannedAPI/Program.cs
@@ -0,0 +1,7 @@
+﻿var myObj = new MyObject();
+MyObject.AMethod(myObj);
+if (myObj)
+{
+    Console.WriteLine("test 1");
+}
+Console.WriteLine("test");
